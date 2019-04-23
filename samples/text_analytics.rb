9294d405e3f89e698d9810069d55fdb4784b8665
Tarun Sharma <49730242+tasharm@users.noreply.github.com>
2019-04-22 19:47:46 -0700

    Update text_analytics.rb

--- samples/text_analytics.rb
+++ samples/text_analytics.rb
@@ -2,148 +2,78 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 
-require 'azure_cognitiveservices_textanalytics'
-
-include Azure::CognitiveServices::TextAnalytics::V2_0::Models
+require 'net/https'
+require 'uri'
+require 'json'
 
 class TextAnalyticsClient
-  def initialize(subscription_key)
-    cognitive_services_credentials =
-        MsRestAzure::CognitiveServicesCredentials.new(subscription_key)
-    @text_analytics_latest_profile_client_obj =
-        Azure::TextAnalytics::Profiles::Latest::Client.new({
-            credentials: cognitive_services_credentials
-        })
-    @text_analytics_latest_profile_client_obj.azure_region = AzureRegions::Westus2
+  @@accessKey
+  @@uri
+  def initialize(accessKey, uri)
+    @@accessKey = accessKey
+    @@uri = uri
   end
 
-  def perform_sample_text_analytics
-    puts "\t\t\tCognitive Services Text Analytics Scenarios"
-    puts "\t\t\t==========================================="
-    puts
+  def SendRequest(path, documents)
+    uri = URI(@@uri + path)
 
-    detect_language_input
-    puts
-    sleep(5)
+    puts 'Please wait a moment for the results to appear.'
 
-    extract_key_phrases
-    puts
-    sleep(5)
+    request = Net::HTTP::Post.new(uri)
+    request['Content-Type'] = "application/json"
+    request['Ocp-Apim-Subscription-Key'] = @@accessKey
+    request.body = documents.to_json
 
-    perform_sentiment_analysis
-    puts
-    sleep(5)
-  end
-
-  private
-  def detect_language_input
-    puts 'Scenario I - Detect Language Input'
-    puts '=================================='
-    puts 'This will detect the languages of the inputs.'
-
-    input_1 = Input.new
-    input_1.id = '1'
-    input_1.text = 'This is a document written in English.'
-
-    input_2 = Input.new
-    input_2.id = '2'
-    input_2.text = 'Este es un document escrito en Español..'
-
-    input_3 = Input.new
-    input_3.id = '3'
-    input_3.text = '这是一个用中文写的文件'
-
-    batch_input = BatchInput.new
-    batch_input.documents = [input_1, input_2, input_3]
-
-    result = @text_analytics_latest_profile_client_obj.detect_language(batch_input)
-
-    if (!result.nil? && !result.documents.nil? && result.documents.length > 0)
-      result.documents.each do |document|
-        puts "Document ID: #{document.id} , Language: #{document.detected_languages[0].name}"
-      end
-    else
-      puts 'No results data..'
+    response = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
+        http.request (request)
     end
-  end
 
-  def extract_key_phrases
-    puts 'Scenario II - Extract Key Phrases'
-    puts '================================='
-    puts 'This will extract key phrases from the sentences.'
-
-    input_1 = MultiLanguageInput.new
-    input_1.id = '1'
-    input_1.language = 'ja'
-    input_1.text = '猫は幸せ'
-
-    input_2 = MultiLanguageInput.new
-    input_2.id = '2'
-    input_2.language = 'de'
-    input_2.text = 'Fahrt nach Stuttgart und dann zum Hotel zu Fu.'
-
-    input_3 = MultiLanguageInput.new
-    input_3.id = '3'
-    input_3.language = 'en'
-    input_3.text = 'My cat is stiff as a rock.'
-
-    input_4 = MultiLanguageInput.new
-    input_4.id = '4'
-    input_4.language = 'es'
-    input_4.text = 'A mi me encanta el fútbol!'
-
-    multi_language_batch_input =  MultiLanguageBatchInput.new
-    multi_language_batch_input.documents = [input_1, input_2, input_3, input_4]
-
-    result = @text_analytics_latest_profile_client_obj.key_phrases(multi_language_batch_input)
-
-    if (!result.nil? && !result.documents.nil? && result.documents.length > 0)
-      result.documents.each do |document|
-        puts "Document Id: #{document.id}"
-        puts '  Key Phrases'
-        document.key_phrases.each do |key_phrase|
-          puts "    #{key_phrase}"
-        end
-      end
-    else
-      puts 'No results data..'
-    end
-  end
-
-  def perform_sentiment_analysis
-    puts 'Scenario III - Perform Sentiment Analysis'
-    puts '========================================='
-    puts 'This will perform sentiment analysis on the sentences.'
-
-    input_1 = MultiLanguageInput.new
-    input_1.id = '1'
-    input_1.language = 'en'
-    input_1.text = 'I had the best day of my life.'
-
-    input_2 = MultiLanguageInput.new
-    input_2.id = '2'
-    input_2.language = 'en'
-    input_2.text = 'This was a waste of my time. The speaker put me to sleep.'
-
-    input_3 = MultiLanguageInput.new
-    input_3.id = '3'
-    input_3.language = 'es'
-    input_3.text = 'No tengo dinero ni nada que dar...'
-
-    input_4 = MultiLanguageInput.new
-    input_4.id = '4'
-    input_4.language = 'it'
-    input_4.text = "L'hotel veneziano era meraviglioso. È un bellissimo pezzo di architettura."
-
-    multi_language_batch_input =  MultiLanguageBatchInput.new
-    multi_language_batch_input.documents = [input_1, input_2, input_3, input_4]
-
-    result = @text_analytics_latest_profile_client_obj.sentiment(multi_language_batch_input)
-
-    if (!result.nil? && !result.documents.nil? && result.documents.length > 0)
-      result.documents.each do |document|
-        puts "Document Id: #{document.id}: Sentiment Score: #{document.score}"
-      end
-    end
+    puts JSON::pretty_generate (JSON (response.body))
   end
 end
+
+def Detect_Language(textAnalyticsClient)
+  documents = { 'documents': [
+    { 'id' => '1', 'text' => 'This is a document written in English.' },
+    { 'id' => '2', 'text' => 'Este es un document escrito en Español.' },
+    { 'id' => '3', 'text' => '这是一个用中文写的文件' }
+  ]}
+  path = 'languages'
+  textAnalyticsClient.SendRequest(path, documents)
+end
+
+def Analyze_Sentiment(textAnalyticsClient)
+  documents = { 'documents': [
+    { 'id' => '1', 'language' => 'en', 'text' => 'I really enjoy the new XBox One S. It has a clean look, it has 4K/HDR resolution and it is affordable.' },
+    { 'id' => '2', 'language' => 'es', 'text' => 'Este ha sido un dia terrible, llegué tarde al trabajo debido a un accidente automobilistico.' }
+  ]}
+  path = 'sentiment'
+  textAnalyticsClient.SendRequest(path, documents)
+end
+
+def Extract_KeyPhrases(textAnalyticsClient)
+  documents = { 'documents': [
+    { 'id' => '1', 'language' => 'en', 'text' => 'I really enjoy the new XBox One S. It has a clean look, it has 4K/HDR resolution and it is affordable.' },
+    { 'id' => '2', 'language' => 'es', 'text' => 'Si usted quiere comunicarse con Carlos, usted debe de llamarlo a su telefono movil. Carlos es muy responsable, pero necesita recibir una notificacion si hay algun problema.' },
+    { 'id' => '3', 'language' => 'en', 'text' => 'The Grand Hotel is a new hotel in the center of Seattle. It earned 5 stars in my review, and has the classiest decor I\'ve ever seen.' },
+  ]}
+  path = 'keyphrases'
+  textAnalyticsClient.SendRequest(path, documents)
+end
+
+def Recognize_Entities(textAnalyticsClient)
+  documents = { 'documents': [
+    { 'id' => '1', 'language' => 'en', 'text' => 'Microsoft is an It company.' }
+  ]}
+  path = 'entities'
+  textAnalyticsClient.SendRequest(path, documents)
+end
+
+accessKey = String.new("enter key here")
+uri = String.new("https://westus.api.cognitive.microsoft.com/text/analytics/v2.1/")
+textAnalyticsClient = TextAnalyticsClient.new(accessKey, uri)
+
+Detect_Language(textAnalyticsClient)
+Analyze_Sentiment(textAnalyticsClient)
+Extract_KeyPhrases(textAnalyticsClient)
+Recognize_Entities(textAnalyticsClient)
