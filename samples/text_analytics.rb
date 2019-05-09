d05f4778a2d8dc587c54f03590ecd9b853c9cf73
Tarun Sharma <49730242+tasharm@users.noreply.github.com>
2019-05-09 13:13:28 -0700

    Added class

--- samples/text_analytics.rb
+++ samples/text_analytics.rb
@@ -1,21 +1,100 @@
-# encoding: utf-8
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License. See License.txt in the project root for license information.
-
 require 'azure_cognitiveservices_textanalytics'
 
 include Azure::CognitiveServices::TextAnalytics::V2_1::Models
 
-credentials =
-    MsRestAzure::CognitiveServicesCredentials.new("enter key here")
-# Replace 'westus' with the correct region for your Text Analytics subscription
-endpoint = String.new("https://westus.api.cognitive.microsoft.com/")
+class TextAnalyticsClient
+  @@textAnalyticsClient
 
-textAnalyticsClient =
-    Azure::TextAnalytics::Profiles::Latest::Client.new({
-                                                           credentials: credentials
-                                                       })
-textAnalyticsClient.endpoint = endpoint
+  def initialize(endpoint, key)
+    credentials =
+        MsRestAzure::CognitiveServicesCredentials.new(key)
+
+    endpoint = String.new(endpoint)
+
+    @@textAnalyticsClient = Azure::TextAnalytics::Profiles::Latest::Client.new({
+        credentials: credentials
+    })
+    @@textAnalyticsClient.endpoint = endpoint
+  end
+
+  def AnalyzeSentiment(inputDocuments)
+    result = @@textAnalyticsClient.sentiment(
+        multi_language_batch_input: inputDocuments
+    )
+
+    if (!result.nil? && !result.documents.nil? && result.documents.length > 0)
+      puts '===== SENTIMENT ANALYSIS ====='
+      result.documents.each do |document|
+        puts "Document Id: #{document.id}: Sentiment Score: #{document.score}"
+      end
+    end
+    puts ''
+  end
+
+  def DetectLanguage(inputDocuments)
+    result = @@textAnalyticsClient.detect_language(
+        language_batch_input: inputDocuments
+    )
+
+    if (!result.nil? && !result.documents.nil? && result.documents.length > 0)
+      puts '===== LANGUAGE DETECTION ====='
+      result.documents.each do |document|
+        puts "Document ID: #{document.id} , Language: #{document.detected_languages[0].name}"
+      end
+    else
+      puts 'No results data..'
+    end
+    puts ''
+  end
+
+  def RecognizeEntities(inputDocuments)
+    result = @@textAnalyticsClient.entities(
+        multi_language_batch_input: inputDocuments
+    )
+
+    if (!result.nil? && !result.documents.nil? && result.documents.length > 0)
+      puts '===== ENTITY RECOGNITION ====='
+      result.documents.each do |document|
+        puts "Document ID: #{document.id}"
+          document.entities.each do |entity|
+            puts "\tName: #{entity.name}, \tType: #{entity.type == nil ? "N/A": entity.type},\tSub-Type: #{entity.sub_type == nil ? "N/A": entity.sub_type}"
+            entity.matches.each do |match|
+              puts "\tOffset: #{match.offset}, \Length: #{match.length},\tScore: #{match.entity_type_score}"
+            end
+            puts
+          end
+      end
+    else
+      puts 'No results data..'
+    end
+    puts ''
+  end
+
+  def ExtractKeyPhrases(inputDocuments)
+    result = @@textAnalyticsClient.key_phrases(
+        multi_language_batch_input: inputDocuments
+    )
+
+    if (!result.nil? && !result.documents.nil? && result.documents.length > 0)
+      puts '===== KEY PHRASE EXTRACTION ====='
+      result.documents.each do |document|
+        puts "Document Id: #{document.id}"
+        puts '  Key Phrases'
+        document.key_phrases.each do |key_phrase|
+          puts "    #{key_phrase}"
+        end
+      end
+    else
+      puts 'No results data..'
+    end
+    puts ''
+  end
+end
+
+# Replace 'westus' with the correct region for your Text Analytics subscription
+endpoint = "https://westus.api.cognitive.microsoft.com/"
+key = "enter key here"
+client = TextAnalyticsClient.new(endpoint, key)
 
 def SentimentAnalysisExample(client)
   # The documents to be analyzed. Add the language of the document. The ID can be any value.
@@ -39,50 +118,30 @@ def SentimentAnalysisExample(client)
   input_4.language = 'it'
   input_4.text = "L'hotel veneziano era meraviglioso. È un bellissimo pezzo di architettura."
 
-  input_documents = MultiLanguageBatchInput.new
-  input_documents.documents = [input_1, input_2, input_3, input_4]
+  inputDocuments =  MultiLanguageBatchInput.new
+  inputDocuments.documents = [input_1, input_2, input_3, input_4]
 
-  result = client.sentiment(
-      multi_language_batch_input: input_documents
-  )
-
-  if (!result.nil? && !result.documents.nil? && result.documents.length > 0)
-    result.documents.each do |document|
-      puts "Document Id: #{document.id}: Sentiment Score: #{document.score}"
-    end
-  end
+  client.AnalyzeSentiment(inputDocuments)
 end
 
 def DetectLanguageExample(client)
-  # The documents to be analyzed.
-  language_input_1 = LanguageInput.new
-  language_input_1.id = '1'
-  language_input_1.text = 'This is a document written in English.'
+ # The documents to be analyzed.
+ language_input_1 = LanguageInput.new
+ language_input_1.id = '1'
+ language_input_1.text = 'This is a document written in English.'
 
-  language_input_2 = LanguageInput.new
-  language_input_2.id = '2'
-  language_input_2.text = 'Este es un document escrito en Español..'
+ language_input_2 = LanguageInput.new
+ language_input_2.id = '2'
+ language_input_2.text = 'Este es un document escrito en Español..'
 
-  language_input_3 = LanguageInput.new
-  language_input_3.id = '3'
-  language_input_3.text = '这是一个用中文写的文件'
+ language_input_3 = LanguageInput.new
+ language_input_3.id = '3'
+ language_input_3.text = '这是一个用中文写的文件'
 
-  input_documents = LanguageBatchInput.new
-  input_documents.documents = [language_input_1, language_input_2, language_input_3]
+ language_batch_input = LanguageBatchInput.new
+ language_batch_input.documents = [language_input_1, language_input_2, language_input_3]
 
-
-  result = client.detect_language(
-      language_batch_input: input_documents
-  )
-
-  if (!result.nil? && !result.documents.nil? && result.documents.length > 0)
-    puts '===== LANGUAGE DETECTION ====='
-    result.documents.each do |document|
-      puts "Document ID: #{document.id} , Language: #{document.detected_languages[0].name}"
-    end
-  else
-    puts 'No results data..'
-  end
+ client.DetectLanguage(language_batch_input)
 end
 
 def RecognizeEntitiesExample(client)
@@ -97,28 +156,10 @@ def RecognizeEntitiesExample(client)
   input_2.language = 'es'
   input_2.text = 'La sede principal de Microsoft se encuentra en la ciudad de Redmond, a 21 kilómetros de Seattle.'
 
-  input_documents = MultiLanguageBatchInput.new
-  input_documents.documents = [input_1, input_2]
+  multi_language_batch_input =  MultiLanguageBatchInput.new
+  multi_language_batch_input.documents = [input_1, input_2]
 
-  result = client.entities(
-      multi_language_batch_input: input_documents
-  )
-
-  if (!result.nil? && !result.documents.nil? && result.documents.length > 0)
-    puts '===== ENTITY RECOGNITION ====='
-    result.documents.each do |document|
-      puts "Document ID: #{document.id}"
-      document.entities.each do |entity|
-        puts "\tName: #{entity.name}, \tType: #{entity.type == nil ? "N/A" : entity.type},\tSub-Type: #{entity.sub_type == nil ? "N/A" : entity.sub_type}"
-        entity.matches.each do |match|
-          puts "\tOffset: #{match.offset}, \Length: #{match.length},\tScore: #{match.entity_type_score}"
-        end
-        puts
-      end
-    end
-  else
-    puts 'No results data..'
-  end
+  client.RecognizeEntities(multi_language_batch_input)
 end
 
 def KeyPhraseExtractionExample(client)
@@ -143,27 +184,13 @@ def KeyPhraseExtractionExample(client)
   input_4.language = 'es'
   input_4.text = 'A mi me encanta el fútbol!'
 
-  input_documents = MultiLanguageBatchInput.new
+  input_documents =  MultiLanguageBatchInput.new
   input_documents.documents = [input_1, input_2, input_3, input_4]
 
-  result = client.key_phrases(
-      multi_language_batch_input: input_documents
-  )
-
-  if (!result.nil? && !result.documents.nil? && result.documents.length > 0)
-    result.documents.each do |document|
-      puts "Document Id: #{document.id}"
-      puts '  Key Phrases'
-      document.key_phrases.each do |key_phrase|
-        puts "    #{key_phrase}"
-      end
-    end
-  else
-    puts 'No results data..'
-  end
+  client.ExtractKeyPhrases(input_documents)
 end
 
-SentimentAnalysisExample(textAnalyticsClient)
-DetectLanguageExample(textAnalyticsClient)
-RecognizeEntitiesExample(textAnalyticsClient)
-KeyPhraseExtractionExample(textAnalyticsClient)
+DetectLanguageExample(client)
+SentimentAnalysisExample(client)
+RecognizeEntitiesExample(client)
+KeyPhraseExtractionExample(client)
