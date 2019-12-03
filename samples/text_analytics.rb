230c45c9b0a2e4408f7d0a0aa4fed125a8b7301d
aahill <aahi@microsoft.com>
2019-12-03 09:43:20 -0800

    Update text_analytics.rb

--- samples/text_analytics.rb
+++ samples/text_analytics.rb
@@ -1,3 +1,7 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+
 # <includeStatement>
 require 'azure_cognitiveservices_textanalytics'
 include Azure::CognitiveServices::TextAnalytics::V2_1::Models
